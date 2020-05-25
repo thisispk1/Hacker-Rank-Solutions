81bb7607515335dceace781b6b4bc3147beb6c07
thisispk1 <65855441+thisispk1@users.noreply.github.com>
2020-05-25 11:34:30 +0530

    Add files via upload

---
+++ Day 2_Operators.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(mc, tp, tx):
+    print(round(mc+(mc*tp/100)+(mc*tx/100)))
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)

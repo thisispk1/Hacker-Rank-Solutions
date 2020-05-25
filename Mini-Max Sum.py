893a5c7d8cb63e844d2f143053fddb3cd9531327
thisispk1 <65855441+thisispk1@users.noreply.github.com>
2020-05-25 11:49:46 +0530

    Add files via upload

---
+++ Mini-Max Sum.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    arr.sort()
+    print(sum(arr[:4]),sum(arr[-4:]))
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)

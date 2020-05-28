2037a56a302cfdc159075598d60009a7bf436e38
Pushkar Vijay Patil <65855441+thisispk1@users.noreply.github.com>
2020-05-28 15:58:20 +0530

    Add files via upload

---
+++ Day 5_Loops.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,10+1):
+        print(str(n)+' x '+str(i)+' =',n*i)

97a45bbf21ba72e1da8ac595bc0811c7669dc208
Pushkar Vijay Patil <65855441+thisispk1@users.noreply.github.com>
2020-06-25 02:28:11 +0530

    Update Time Conversion.py

--- Time Conversion.py
+++ Time Conversion.py
@@ -1,20 +1,23 @@
 def timeConversion(s):
     if s.endswith('PM') or s.endswith('pm'):
         x=s.split(':')
-        y=int(x[0])
-        y=y+12
-        x[0]=str(y)
-        x=str(':'.join(x))
-        return(x[:8])
+        if x[0].startswith('12'):
+            return(s[:8])
+        else:
+            y=int(x[0])
+            y=y+12
+            x[0]=str(y)
+            x=str(':'.join(x))
+            return(x[:8])
     elif s.endswith('AM') or s.endswith('am'):
         if s.startswith('12'):
             x=s.split(':')
             y=int(x[0])
             y=y-12
-            x[0]=str(y)
+            x[0]=str('0'+str(y))
             x=str(':'.join(x))
             return(x[:8])
         else:
             return(s[:8])
 t=str(input('t='))
-timeConversion(t)
+print(timeConversion(t))
